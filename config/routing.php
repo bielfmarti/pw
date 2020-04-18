e2e75b6e0a5cd6484fc50a833d8123375f4f26c1
Biel Fernández Martí <b.fernandez@students.salle.url.edu>
2020-04-18 19:36:18 +0000

    Afegit controller

--- config/routing.php
+++ config/routing.php
@@ -1,5 +1,30 @@
 <?php
 
+use \SallePW\SlimApp\Controller\HomeController;
+
+$app->get('/', HomeController::class . ':showHomePage')->setName('home');
+
+
+
+
+
+
+/*
+ * Amb controller, aquest hauria de ser el codi, quan funcioni la classe HomeController, posa aquest codi
+ *
+<?php
+
+use \SallePW\SlimApp\Controller\HomeController;
+
+$app->get('/', HomeController::class . ':showHomePage')->setName('home');
+ */
+
+
+
+
+/*
+<?php
+
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
@@ -9,3 +34,6 @@ $app->get(
         return $this->get('view')->render($response, 'home.twig', []);
     }
 )->setName('home');
+
+ *
+ */
