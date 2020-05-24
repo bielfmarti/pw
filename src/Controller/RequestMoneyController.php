b7d00556c7749318cd78232376036aa196962ed2
alex.valle <alex.valle@students.salle.url.edu>
2020-05-24 13:13:00 +0200

    sembla q tot esta acabat

--- src/Controller/RequestMoneyController.php
+++ src/Controller/RequestMoneyController.php
@@ -75,6 +75,35 @@ final class RequestMoneyController
 
         $success = "money requested!";
 
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $statement = $db->query("SELECT USER.money, USER.id FROM USER WHERE email LIKE '$email'" );
+        $statement->execute();
+        $info = $statement->fetch();
+
+        $money = $info[0];
+        $id = $info[1];
+
+        $statement = $db->query("SELECT TRANSACTION.id_sender, TRANSACTION.id_reciever, TRANSACTION.money, TRANSACTION.type FROM TRANSACTION WHERE id_sender LIKE '$id' OR id_reciever LIKE '$id' ORDER BY id DESC LIMIT 5" );
+        $statement->execute();
+        $info = $statement->fetchAll();
+
+        $i = 0;
+        $arg[] = 0;
+
+
+        while (!empty($info[$i][0])) {
+            $arg[$i] = "";
+            $arg[$i] .= "Id of the sender: " . $info[$i][0];
+            $arg[$i] .= " | Id of the reciever: " . $info[$i][1];
+            $arg[$i] .= " | Money: " . $info[$i][2];
+            $arg[$i] .= " | Type: " . $info[$i][3];
+
+
+            $i++;
+
+        }
+
         return $this->container->get('view')->render(
             $response,
             'dashboard.twig',
@@ -84,6 +113,8 @@ final class RequestMoneyController
               'sixDigits' => $_SESSION['sixDigits'],
               'transactions' => "",
               'success' => $success,
+              'i' => $i,
+              'message' => $arg,
             ]
         );
 
